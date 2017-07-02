22480798828f8f8c9586293cafc908c3fb576da7
Joshua Pratt <jp10010101010000@gmail.com>
2017-07-02 11:59:44 +1000

    Added exit 0

--- wakeup.sh
+++ wakeup.sh
@@ -4,10 +4,11 @@
 # Copyright (C) 2017 cypher <cypher@Cortana>
 
 ACPIFILE=/proc/acpi/wakeup
-
 SIGNALS=(XHC1 LID0)
 
 for SIGNAL in $SIGNALS
 do
   grep $SIGNAL $ACPIFILE | grep enabled > /dev/null && echo $SIGNAL > $ACPIFILE
 done
+
+exit 0
